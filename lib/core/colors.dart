08e0d80c344bda308125afe1015dbcbe71d342a4
Ayuba Oladele Salami <olayubasalami@gmail.com>
2026-02-14 23:53:22 +0100

    created app colors

---
+++ lib/core/colors.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFF7F3DFF);
+  static const Color primaryLight = Color(0xFF9D62FF);
+  static const Color primaryDark = Color(0xFF5E2DD8);
+
+  static const Color secondary = Color(0xFF00D09E);
+  static const Color secondaryLight = Color(0xFF33DEAE);
+  static const Color secondaryDark = Color(0xFF00A67C);
+
+  static const Color background = Color(0xFFF8F9FA);
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color surfaceDark = Color(0xFF1E1E1E);
+
+  static const Color textPrimary = Color(0xFF212529);
+  static const Color textSecondary = Color(0xFF6C757D);
+  static const Color textHint = Color(0xFFADB5BD);
+
+  static const Color income = Color(0xFF00D09E);
+  static const Color expense = Color(0xFFFF6B6B);
+
+  static const Color success = Color(0xFF51CF66);
+  static const Color warning = Color(0xFFFFA94D);
+  static const Color error = Color(0xFFFF6B6B);
+  static const Color info = Color(0xFF4DABF7);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color grey100 = Color(0xFFF8F9FA);
+  static const Color grey200 = Color(0xFFE9ECEF);
+  static const Color grey300 = Color(0xFFDEE2E6);
+  static const Color grey400 = Color(0xFFCED4DA);
+  static const Color grey500 = Color(0xFFADB5BD);
+  static const Color grey600 = Color(0xFF6C757D);
+  static const Color grey700 = Color(0xFF495057);
+  static const Color grey800 = Color(0xFF343A40);
+  static const Color grey900 = Color(0xFF212529);
+}
